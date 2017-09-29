dddde571afbc91f4063a75b03aa0e023b504fa7f
Jiyun <brightnk@outlook.com>
2017-09-28 18:06:46 -0700

    add form module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+
 import {AppComponent} from './app.component';
 import {MediaItemComponent} from './media-item.component';
 import {MediaItemListComponent} from './media-item-list.component';
 import {FavoriteDirective} from './favorite.directive';
 import {CategoryListPipe} from './category-list.pipe';
+import {MediaItemFormComponent} from './media-item-form.component';
 
 @NgModule({
 		
 	imports: [
-		BrowserModule
+		BrowserModule,
+		FormsModule
 
 
 	],
@@ -18,7 +23,8 @@ import {CategoryListPipe} from './category-list.pipe';
 		MediaItemComponent,
 		MediaItemListComponent,
 		FavoriteDirective,
-		CategoryListPipe
+		CategoryListPipe,
+		MediaItemFormComponent
 	],
 	bootstrap: [
 		AppComponent
