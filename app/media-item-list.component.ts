aa1cb433064fcf48731d8b84315da744e0805e96
Jiyun <brightnk@outlook.com>
2017-10-15 19:38:21 -0700

    compelte router template

--- app/media-item-list.component.ts
+++ app/media-item-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MediaItemService} from './media-item.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'mw-media-item-list',
@@ -10,12 +11,28 @@ import { MediaItemService} from './media-item.service';
 export class MediaItemListComponent {
   medium = '';
   mediaItems = [];
+  paramsSubscription;
 
-  constructor(private mediaItemService: MediaItemService){}
+  constructor(private mediaItemService: MediaItemService,
+    private activatedRoute: ActivatedRoute){}
 
 
   ngOnInit(){
-    this.getMediaItems(this.medium);
+    //this.getMediaItems(this.medium);
+    this.paramsSubscription = this.activatedRoute.params
+    .subscribe(params =>{
+      let medium = params['medium'];
+      if(medium.toLowerCase()==='all'){
+        medium ="";
+      }
+      this.getMediaItems(medium);
+
+    });
+  }
+
+
+  ngOnDestory(){
+    this.paramsSubscription.unsubscribe();
   }
 
 
